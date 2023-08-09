e12eba7be90d70f2a6681298ac966c7f7c8cc960
Bogdan Scher <bogdan2401drg@gmail.com>
2023-08-09 21:19:10 +0300

    Client changes v1

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,12 +3,18 @@ import React from "react";
 import hStyle from "./Header.module.scss";
 import HeaderTop from "./HeaderTop/HeaderTop";
 import HeaderBottom from "./HeaderBottom/HeaderBottom";
+import { Outlet } from "react-router-dom";
 
 const Header = (props) => {
-  return <header className={hStyle.headerMain}>
-    <HeaderTop />
-    <HeaderBottom />
-  </header>;
+  return (
+    <>
+      <header className={hStyle.headerMain}>
+        <HeaderTop />
+        <HeaderBottom />
+      </header>
+      <Outlet />
+    </>
+  );
 };
 
 export default Header;
--- src/Components/Header/HeaderTop/HeaderTop.jsx
+++ src/Components/Header/HeaderTop/HeaderTop.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import hTopStyles from"./HeaderTop.module.scss";
 import jordanLogo from "../../../Assets/Img/HeaderImg/JordanLogo.webp";
 import { Link } from "react-router-dom";
+import AdminButtons from "../Navigation/AdminButtons";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../../redux/slices/auth";
+import { selectIsAuth } from "../../../redux/slices/auth";
 
 
 const HeaderTop = (props) => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  const onClickLogout = () => {
+    if (window.confirm("Are you sure you want to?")) {
+      dispatch(logout());
+      window.localStorage.removeItem("token");
+    }
+  };
   return (
     <div className={hTopStyles.headerTop}>
       <div className={hTopStyles.container}>
@@ -15,18 +28,36 @@ const HeaderTop = (props) => {
             src={jordanLogo}
           ></img>
           <div className={hTopStyles.headerTopMain__navContainer}>
-            <div className={hTopStyles.navC}> 
-              <Link to="/findastore" className={hTopStyles.navTextD}>Find A Store</Link>
+            <div className={hTopStyles.navC}>
+              <Link to="/findastore" className={hTopStyles.navTextD}>
+                Find A Store
+              </Link>
             </div>
             <div className={hTopStyles.navC}>
-              <Link to="/Help" className={hTopStyles.navTextD}>Help</Link>
+              <Link to="/Help" className={hTopStyles.navTextD}>
+                Help
+              </Link>
             </div>
-            <div className={hTopStyles.navC}>
-              <Link to="/JoinUs" className={hTopStyles.navTextD}>Join Us</Link>
-            </div>
-            <div className={hTopStyles.navC}>
-              <Link to="/SignIn" className={hTopStyles.navTextD}>Sign In</Link>
-            </div>
+            {isAuth ? (
+              <>
+                <div className={hTopStyles.navC}>
+                  <AdminButtons isAuth={isAuth} onClickLogout={onClickLogout}/>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className={hTopStyles.navC}>
+                  <Link to="/login" className={hTopStyles.navTextD}>
+                    Log In
+                  </Link>
+                </div>
+                <div className={hTopStyles.navC}>
+                  <Link to="/register" className={hTopStyles.navTextD}>
+                    Sign In
+                  </Link>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
